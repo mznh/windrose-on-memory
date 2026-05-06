370833580396d6a1583dfa9f45bfb0eaffc72e21
mznh <hiromtym@gmail.com>
2026-05-06 23:30:43 +0900

    read uid from env

--- data-loader/load.sh
+++ data-loader/load.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 # load.sh - 初期データのRAMディスクへの流し込み
 
+APP_UID=$PUID
+APP_GID=$PGID
+
 echo '[Loader] Checking /backup directory...'
 
 if [ "$(ls -A /backup 2>/dev/null)" ]; then
     echo '[Loader] Backup data found. Restoring to RAM...'
-    rsync -v -rltD --chown=1002:1002 /backup/ /saved/ || echo '[Loader] rsync ERROR!'
+    rsync -v -rltD --chown=${APP_UID}:${APP_GID} /backup/ /saved/ || echo '[Loader] rsync ERROR!'
 else
     echo '[Loader] No backup found. Fresh start.'
 fi
 
 # data-sync側が先に実行されてしまうことを防ぐマーカーファイル
 touch /saved/.ram_loaded
-chown 1002:1002 /saved/.ram_loaded
+chown ${APP_UID}:${APP_GID} /saved/.ram_loaded
 
 echo '[Loader] Finished. Marker created.'
